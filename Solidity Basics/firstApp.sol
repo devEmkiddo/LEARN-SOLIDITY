1760159833a17703d93ba09a4281e692e03bb534
devEmiddo <149944165+devEmkiddo@users.noreply.github.com>
2024-01-02 12:40:30 +0100

    Add files via upload

---
+++ Solidity Basics/firstApp.sol
@@ -0,0 +1,23 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+contract Counter {
+    //Initial a variable to store our count
+    uint256 public count;
+
+    // Function to get the current count
+    function get() public view returns (uint) {
+        return count;
+    }
+
+    // Function to increment count by 1
+    function inc() public {
+        count += 1;
+    }
+
+    // Function to decrement count by 1
+    function dec() public {
+        // This function will fail if count = 0
+        count -= 1;
+    }
+}
